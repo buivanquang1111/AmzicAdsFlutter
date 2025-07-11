1cc999e461d13536eaae02e426768394470a19cb
quangbv <quangbv@vtdglobal.com>
2025-07-11 14:29:40 +0700

    add text dialog loading

--- lib/dialog/loading_dialog.dart
+++ lib/dialog/loading_dialog.dart
@@ -8,9 +8,23 @@ class LoadingDialog extends StatelessWidget {
     return PopScope(
       canPop: false,
       child: Container(
-        color: Colors.black,
+        color: Colors.white,
         child: Center(
-          child: CircularProgressIndicator(),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircularProgressIndicator(color: Colors.black),
+              const SizedBox(height: 12),
+              Text(
+                'Loading Ads',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
