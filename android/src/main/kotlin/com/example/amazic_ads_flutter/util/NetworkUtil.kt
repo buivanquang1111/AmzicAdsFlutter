c127c8693e1fad15af9abb1391c99abb49aa2997
quangbv <quangbv@vtdglobal.com>
2025-08-07 14:31:07 +0700

    check inter android 9

--- android/src/main/kotlin/com/example/amazic_ads_flutter/util/NetworkUtil.kt
+++ android/src/main/kotlin/com/example/amazic_ads_flutter/util/NetworkUtil.kt
@@ -1,22 +1,32 @@
 package com.example.amazic_ads_flutter.util
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.os.Build
 
 class NetworkUtil {
     fun isConnectedNetwork(context: Context?): Boolean {
         if (context == null) return false
+
         val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
-        val network = manager?.activeNetwork
-        val capabilities = manager?.getNetworkCapabilities(network)
-        return capabilities != null && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
+            ?: return false
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val network = manager.activeNetwork ?: return false
+            val capabilities = manager.getNetworkCapabilities(network) ?: return false
+
+            return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                    (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
+        } else {
+            val activeNetworkInfo = manager.activeNetworkInfo
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        }
     }
 
     fun isNetworkActive(context: Context?): Boolean {
-        return context != null && isConnectedNetwork(context)
+        return isConnectedNetwork(context)
     }
 }
