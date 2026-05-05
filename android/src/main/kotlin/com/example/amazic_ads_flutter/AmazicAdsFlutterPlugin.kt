a692a6dda725e2169bfd33e025aea68a8b112ad3
quangbv <quangbv@vtdglobal.com>
2026-05-05 15:45:04 +0700

    check onpaidevent

--- android/src/main/kotlin/com/example/amazic_ads_flutter/AmazicAdsFlutterPlugin.kt
+++ android/src/main/kotlin/com/example/amazic_ads_flutter/AmazicAdsFlutterPlugin.kt
@@ -204,7 +204,7 @@ class AmazicAdsFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
                         override fun onPaidEvent(ad: InterstitialAd, adValue: AdValue) {
                             val network = ad.responseInfo.loadedAdapterResponseInfo?.adSourceName
-                            val valueMicros = adValue.valueMicros.toDouble()
+                            val valueMicros = adValue.valueMicros
                             val currencyCode = adValue.currencyCode
 
                             Log.d("Admob_ads", "onPaidEvent: inter - network = $network - valueMicros = $valueMicros - currentCode = $currencyCode")
